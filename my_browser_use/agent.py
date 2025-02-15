4b19d8cd3bb78ee7aa3f6e48d495deceb0e5b046
Faiz Ahmed <faizsameerahmed96@gmail.com>
2025-02-15 14:52:47 -0800

    continue session

--- my_browser_use/agent.py
+++ my_browser_use/agent.py
@@ -2,6 +2,7 @@ from browser_use import Agent, Browser, BrowserConfig
 from browser_use.browser.context import BrowserContextConfig
 from langchain_openai import ChatOpenAI
 from playwright.sync_api import BrowserContext, sync_playwright
+from browser_use.controller.service import Controller
 
 
 from my_browser_use.custom_prompt import MySystemPrompt
@@ -10,16 +11,16 @@ profile_path = "/Users/faizahmed/Library/Application Support/Google/Chrome/Defau
 
 
 async def main():
-    browser = Browser(
-        config=BrowserConfig(
-            chrome_instance_path='/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+    browser = Browser()
+    async with await browser.new_context() as context:
+        agent = Agent(
+            task="Open reddit homepage",
+            llm=ChatOpenAI(model="gpt-4o-mini"),
+            controller=Controller(),
+            browser=browser,
+            browser_context=context,
         )
-    )
-    agent = Agent(
-        task="Open reddit homepage",
-        llm=ChatOpenAI(model="gpt-4o"),
-        browser=browser,
-    )
-    result = await agent.run()
-    agent.add_new_task("scroll down")
-    result = await agent.run()
+        result = await agent.run()
+        # agent.browser = browser
+        agent.add_new_task("Scroll down")
+        result = await agent.run()
