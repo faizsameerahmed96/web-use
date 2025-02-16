b32d606e5b9fb89db07467bfaa37491fa5851154
Faiz Ahmed <faizsameerahmed96@gmail.com>
2025-02-16 05:15:33 -0800

    custom actions

--- my_browser_use/agent.py
+++ my_browser_use/agent.py
@@ -10,20 +10,22 @@ from audio.record_audio_optimize import wait_for_wakeword
 from my_browser_use.custom_prompt import MySystemPrompt
 import json
 import asyncio
+import re
 
 profile_path = "/Users/faizahmed/Library/Application Support/Google/Chrome/Default"
 
 controller = Controller()
 
 
-@controller.action(
-    "Ask the user for information in case you need some more information on the tasks."
-)
-def ask_human(question: str) -> str:
-    print(f"############ {question}")
-    user_input = record_audio_and_transcribe()
-    # user_input = input(question)
-    return ActionResult(extracted_content=user_input)
+# @controller.action(
+#     "Ask the user for information in case you need some more information on the tasks."
+# )
+# def ask_human(question: str) -> str:
+#     print(f"############ {question}")
+#     wait_for_wakeword()
+#     user_input = record_audio_and_transcribe()
+#     # user_input = input(question)
+#     return ActionResult(extracted_content=user_input)
 
 
 async def main(message_queue):
@@ -36,9 +38,29 @@ async def main(message_queue):
             wait_for_wakeword()
             await message_queue.put(json.dumps({"state": "RECORDING_AUDIO"}))
             task = record_audio_and_transcribe()
+
+            if "scroll down" == re.sub(r"[^A-Za-z0-9 ]+", "", task.lower()):
+                page = await context.get_current_page()
+                await page.evaluate(
+                    'window.scrollBy({ top: window.innerHeight - 100,left: 0,behavior : "smooth"})'
+                )
+                continue
+
+            if "scroll up" == re.sub(r"[^A-Za-z0-9 ]+", "", task.lower()):
+                page = await context.get_current_page()
+                await page.evaluate(
+                    'window.scrollBy({ top: -window.innerHeight - 100,left: 0,behavior : "smooth"})'
+                )
+                continue
+
+            if "go back" == re.sub(r"[^A-Za-z0-9 ]+", "", task.lower()):
+                page = await context.get_current_page()
+                await page.go_back()
+                continue
+
             print(f"Task: {task}")
             await message_queue.put(json.dumps({"state": "PERFORMING_TASK"}))
-            # task = input("task: ")
+            # task = input("task: ")cl
             if not agent:
                 agent = Agent(
                     task=task,
